2764e1d7a2a75ea27b0d652f597949cf47a974ae
auremompd <auremompd@gmail.com>
2015-09-28 12:38:21 +0300

    Use rethrow to get rid of analyzer warnings.

--- Auremo/Auremo/QuickSearchThread.cs
+++ Auremo/Auremo/QuickSearchThread.cs
@@ -53,7 +53,7 @@ namespace Auremo
                                                "Please take a screenshot of this message and send it to the developer.\n\n" +
                                                e.ToString(),
                                                "Auremo has crashed!");
-                throw e;
+                throw;
             }
         }
 

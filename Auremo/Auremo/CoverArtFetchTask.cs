156bbf3d930ce586b97b5d76fcb36f0eba293651
auremompd <auremompd@gmail.com>
2015-10-05 00:00:32 +0300

    Add missing files.

---
+++ Auremo/Auremo/CoverArtFetchTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class CoverArtFetchTask
+    {
+        public enum RequestType { Fetch, Delete };
+
+        public CoverArtFetchTask(RequestType request, string artist, string album)
+        {
+            Request = request;
+            Artist = artist;
+            Album = album;
+        }
+
+        public RequestType Request
+        {
+            get;
+            private set;
+        }
+
+        public string Artist
+        {
+            get;
+            private set;
+        }
+
+        public string Album
+        {
+            get;
+            private set;
+        }
+    }
+}

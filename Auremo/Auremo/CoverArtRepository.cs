e6f6d0a75ea45540533e1f5019510d1d0c3ba42a
auremompd <auremompd@gmail.com>
2015-10-04 23:59:55 +0300

    Refactor cover art repo.

--- Auremo/Auremo/CoverArtRepository.cs
+++ Auremo/Auremo/CoverArtRepository.cs
@@ -46,18 +46,19 @@ namespace Auremo
         private ImageSource m_CoverLoadingImage = null;
         private ImageSource m_CoverUnavailableImage = null;
         private CoverArtFetchThread m_Fetcher = null;
+
         private Thread m_Thread = null;
         private object m_Lock = new object();
         private ManualResetEvent m_Event = new ManualResetEvent(false);
-        private IList<Tuple<string, string>> m_CoverDeletionRequests = new List<Tuple<string, string>>();
-        private IList<Album> m_HighPriorityRequests = new List<Album>();
-        private IList<Album> m_NormalPriorityRequests = new List<Album>();
-        private IList<Album> m_LowPriorityRequests = new List<Album>();
-        private Album m_CurrentlyRunningRequest = null;
-        private IList<Tuple<Album, ImageSource>> m_CompletedRequests = new List<Tuple<Album, ImageSource>>();
+        private Queue<CoverArtFetchTask> m_CoverDeletionRequests = new Queue<CoverArtFetchTask>();
+        private Queue<CoverArtFetchTask> m_HighPriorityRequests = new Queue<CoverArtFetchTask>();
+        private Queue<CoverArtFetchTask> m_NormalPriorityRequests = new Queue<CoverArtFetchTask>();
+        private Queue<CoverArtFetchTask> m_LowPriorityRequests = new Queue<CoverArtFetchTask>();
+
+        private IList<Tuple<string, string, ImageSource>> m_CompletedRequests = new List<Tuple<string, string, ImageSource>>();
         private bool m_Terminating = false;
 
-        enum Priority
+        public enum Priority
         {
             High, Normal, Low
         };
@@ -71,41 +72,97 @@ namespace Auremo
 
             m_Fetcher = new CoverArtFetchThread(this);
             m_Thread = new Thread(new ThreadStart(m_Fetcher.Start));
-            m_Thread.Name = "Covert art fetch thread";
+            m_Thread.Name = "Cover art fetch thread";
             m_Thread.Start();
         }
-    
-        public void SetCoverOfAlbum(Album album)
-        {
-            FetchCoverIfMissing(album, Priority.Normal);
-        }
 
-        /*
-        public void SetCoversOfEntireDatabase()
+        public ImageSource FetchCoverOfAlbum(Album album, Priority priority)
         {
-            foreach (Album album in m_DataModel.Database.Albums)
+            if (album == null || album.Artist == null || album.Title == null)
             {
-                FetchCoverIfMissing(album, Priority.Low);
+                return null;
+            }
+            else
+            {
+                return FetchCoverOfAlbum(album.Artist.Name, album.Title, priority);
             }
         }
-        */
+
+        public ImageSource FetchCoverOfAlbum(string artist, string album, Priority priority)
+        {
+            if (artist == null || album == null)
+            {
+                return null;
+            }
+            else
+            {
+                EnsureLookupEntryExists(artist);
+
+                if (!m_Covers[artist].ContainsKey(album))
+                {
+                    m_Covers[artist][album] = m_CoverLoadingImage;
+
+                    lock (m_Lock)
+                    {
+                        if (priority == Priority.High)
+                        {
+                            m_HighPriorityRequests.Enqueue(new CoverArtFetchTask(CoverArtFetchTask.RequestType.Fetch, artist, album));
+                        }
+                        else if (priority == Priority.Normal)
+                        {
+                            m_NormalPriorityRequests.Enqueue(new CoverArtFetchTask(CoverArtFetchTask.RequestType.Fetch, artist, album));
+                        }
+                        else
+                        {
+                            m_LowPriorityRequests.Enqueue(new CoverArtFetchTask(CoverArtFetchTask.RequestType.Fetch, artist, album));
+                        }
+                            
+                        m_Event.Set();
+                    }
+                }
+
+                return m_Covers[artist][album];
+            }
+        }
+
+        public void SetCoversOfEntireDatabase()
+        {
+            foreach (Artist artist in m_DataModel.Database.Artists.Values)
+            {
+                foreach (Album album in m_DataModel.Database.Expand(artist))
+                {
+                    FetchCoverOfAlbum(album, Priority.Low);
+                }
+            }
+        }
+
+        public void RemoveCover(Album album)
+        {
+            if (album != null && album.Artist != null && album.Title != null)
+            {
+                RemoveCover(album.Artist.Name, album.Title);
+            }
+        }
 
         public void RemoveCover(string artist, string album)
         {
-            if (m_Covers.ContainsKey(artist))
+            if (artist != null && album != null)
             {
-                IDictionary<string, ImageSource> albums = m_Covers[artist];
-
-                if (albums.ContainsKey(album))
+                if (m_Covers.ContainsKey(artist) && m_Covers[artist].ContainsKey(album))
                 {
-                    albums.Remove(album);
-                }
-            }
+                    m_Covers[artist].Remove(album);
 
-            lock (m_Lock)
-            {
-                m_CoverDeletionRequests.Add(new Tuple<string, string>(artist, album));
-                m_Event.Set();
+                    if (m_Covers[artist].Count == 0)
+                    {
+                        m_Covers.Remove(artist);
+                    }
+
+                    lock (m_Lock)
+                    {
+                        m_CoverDeletionRequests.Enqueue(new CoverArtFetchTask(CoverArtFetchTask.RequestType.Delete, artist, album));
+                        m_Event.Set();
+                    }
+                }
             }
         }
 
@@ -125,75 +182,56 @@ namespace Auremo
             }
         }
 
-        public bool GetRequest(out string artist, out string album, out bool deleteRequested)
+        /// Returns a new fetch or delete request, or null if the worker is to
+        /// terminate. If no requests are pending, block.
+        public CoverArtFetchTask PopRequest()
         {
-            artist = null;
-            album = null;
-            m_CurrentlyRunningRequest = null;
-            deleteRequested = false;
-            bool done = false;
+            CoverArtFetchTask result = null;
 
-            while (!done)
+            while (result == null)
             {
                 lock (m_Lock)
                 {
                     if (m_Terminating)
                     {
-                        return false;
+                        return null;
                     }
                     else
                     {
                         if (m_CoverDeletionRequests.Count > 0)
                         {
-                            m_CurrentlyRunningRequest = null;
-                            artist = m_CoverDeletionRequests[0].Item1;
-                            album = m_CoverDeletionRequests[0].Item2;
-                            m_CoverDeletionRequests.RemoveAt(0);
-                            done = true;
+                            result = m_CoverDeletionRequests.Dequeue();
                         }
-                        if (m_HighPriorityRequests.Count > 0)
+                        else if (m_HighPriorityRequests.Count > 0)
                         {
-                            m_CurrentlyRunningRequest = m_HighPriorityRequests[0];
-                            artist = m_HighPriorityRequests[0].Artist.Name;
-                            album = m_HighPriorityRequests[0].Title;
-                            m_HighPriorityRequests.RemoveAt(0);
-                            done = true;
+                            result = m_HighPriorityRequests.Dequeue();
                         }
                         else if (m_NormalPriorityRequests.Count > 0)
                         {
-                            m_CurrentlyRunningRequest = m_NormalPriorityRequests[0];
-                            artist = m_NormalPriorityRequests[0].Artist.Name;
-                            album = m_NormalPriorityRequests[0].Title;
-                            m_NormalPriorityRequests.RemoveAt(0);
-                            done = true;
+                            result = m_NormalPriorityRequests.Dequeue();
                         }
                         else if (m_LowPriorityRequests.Count > 0)
                         {
-                            m_CurrentlyRunningRequest = m_LowPriorityRequests[0];
-                            artist = m_LowPriorityRequests[0].Artist.Name;
-                            album = m_LowPriorityRequests[0].Title;
-                            m_LowPriorityRequests.RemoveAt(0);
-                            done = true;
+                            result = m_LowPriorityRequests.Dequeue();
                         }
                     }
                 }
 
-                if (!done)
+                if (result == null)
                 {
                     m_Event.WaitOne();
                     m_Event.Reset();
                 }
             }
                         
-            return true;
+            return result;
         }
 
-        public void CoverArtFetchFinished(ImageSource cover)
+        public void CoverArtFetchFinished(string artist, string album, ImageSource cover)
         {
             lock (m_Lock)
             {
-                m_CompletedRequests.Add(new Tuple<Album, ImageSource>(m_CurrentlyRunningRequest, cover));
-                m_CurrentlyRunningRequest = null;
+                m_CompletedRequests.Add(new Tuple<string, string, ImageSource>(artist, album, cover));
             }
 
             m_DataModel.MainWindow.Dispatcher.BeginInvoke((Action)OnCoverArtFetchFinished, null);
@@ -201,104 +239,27 @@ namespace Auremo
 
         private void OnCoverArtFetchFinished()
         {
-            IList<Tuple<Album, ImageSource>> completedRequests = null;
+            IList<Tuple<string, string, ImageSource>> completedRequests = null;
 
             lock (m_Lock)
             {
                 completedRequests = m_CompletedRequests;
-                m_CompletedRequests = new List<Tuple<Album, ImageSource>>();
+                m_CompletedRequests = new List<Tuple<string, string, ImageSource>>();
             }
 
-            foreach (Tuple<Album, ImageSource> request in completedRequests)
+            foreach (Tuple<string, string, ImageSource> request in completedRequests)
             {
-                Album album = request.Item1;
-                ImageSource cover = request.Item2;
-
-                /*
-                album.Cover = cover;
-
-                if (cover == null)
-                {
-                    m_Covers[album.Artist.Name][album.Title] = m_CoverUnavailableImage;
-                }
-                else
-                {
-                    m_Covers[album.Artist.Name][album.Title] = album.Cover;
-                }
-                */
-            }
-        }   
-
-        private void FetchCoverIfMissing(Album album, Priority priority)
-        {
-            if (album == null)
-            {
-                return;
-            }
-            else if (album.Cover != null && album.Cover != m_CoverLoadingImage && album.Cover != m_CoverUnavailableImage)
-            {
-                return;
-            }
-
-            if (!CoverIsSearchable(album))
-            {
-                album.Cover = m_CoverUnavailableImage;
-                return;
-            }
-
-            EnsureLookupEntryExists(album);
-
-            if (m_Covers[album.Artist.Name][album.Title] == null)
-            {
-                m_Covers[album.Artist.Name][album.Title] = m_CoverLoadingImage;
-                EnqueueRequst(album, priority);
+                EnsureLookupEntryExists(request.Item1);
+                m_Covers[request.Item1][request.Item2] = request.Item3 ?? m_CoverUnavailableImage;
             }
         }
 
-        private void EnsureLookupEntryExists(Album album)
+        private void EnsureLookupEntryExists(string artist)
         {
-            if (!m_Covers.ContainsKey(album.Artist.Name))
+            if (!m_Covers.ContainsKey(artist))
             {
-                m_Covers.Add(album.Artist.Name, new SortedDictionary<string, ImageSource>());
+                m_Covers[artist] = new SortedDictionary<string, ImageSource>();
             }
-
-            IDictionary<string, ImageSource> albums = m_Covers[album.Artist.Name];
-
-            if (!albums.ContainsKey(album.Title))
-            {
-                albums[album.Title] = null;
-            }
-        }
-
-        private void EnqueueRequst(Album album, Priority priority)
-        {
-            if (album.Cover == null)
-            {
-                lock (m_Lock)
-                {
-                    if (priority == Priority.High)
-                    {
-                        m_HighPriorityRequests.Add(album);
-                    }
-                    else if (priority == Priority.Normal)
-                    {
-                        m_NormalPriorityRequests.Add(album);
-                    }
-                    else if (priority == Priority.Low)
-                    {
-                        m_LowPriorityRequests.Add(album);
-                    }
-
-                    m_Event.Set();
-                }
-            }
-        }
-
-        private bool CoverIsSearchable(Album album)
-        {
-            return album != null &&
-                album.Artist != null && album.Artist.Name != "" && album.Artist.Name != Artist.Unknown &&
-                album.Title != null && album.Title != "" && album.Title != Album.Unknown;
         }
     }
 }

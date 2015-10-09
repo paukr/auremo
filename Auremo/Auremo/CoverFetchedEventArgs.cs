84fc26635dc07a053295f2ea14a06e35b0eecd8a
auremompd <auremompd@gmail.com>
2015-10-09 20:10:02 +0300

    Add CoverArtFetchedEventArgs.

---
+++ Auremo/Auremo/CoverFetchedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Auremo
+{
+    public class CoverFetchedEventArgs : EventArgs
+    {
+        public CoverFetchedEventArgs(string artist, string album, ImageSource cover)
+        {
+            Artist = artist;
+            Album = album;
+            Cover = cover;
+        }
+
+        public string Artist
+        {
+            get;
+            private set;
+        }
+
+        public string Album
+        {
+            get;
+            private set;
+        }
+
+        public ImageSource Cover
+        {
+            get;
+            private set;
+        }
+    }
+}

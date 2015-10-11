4f4eaaeb965772da89be4f14ab3807da1ec05693
auremompd <auremompd@gmail.com>
2015-10-11 14:02:11 +0300

    Add hooks for volume keys (they won't work either).

--- Auremo/Auremo/GlobalMediaKeys.cs
+++ Auremo/Auremo/GlobalMediaKeys.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace Auremo
@@ -12,51 +11,58 @@ namespace Auremo
 
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr window, int id);
-        
-        private const int WM_KEYDOWN = 0x0100;
+
         private const int WM_HOTKEY = 0x0312;
+        private const int VK_VOLUME_DOWN = 0xAE;
+        private const int VK_VOLUME_UP = 0xAF;
         private const int VK_MEDIA_NEXT_TRACK = 0xB0;
         private const int VK_MEDIA_PREV_TRACK = 0xB1;
         private const int VK_MEDIA_STOP = 0xB2;
         private const int VK_MEDIA_PLAY_PAUSE = 0xB3;
 
+        private const int MediaKeyVolumeUp = 1;
+        private const int MediaKeyVolumeDown = 2;
+        private const int MediaKeyNext = 3;
+        private const int MediaKeyPrevious = 4;
+        private const int MediaKeyStop = 5;
+        private const int MediaKeyPlayPause = 6;
+
         private void RegisterGlobalMediaKeys()
         {
-            /*
             IntPtr handle = new WindowInteropHelper(this).Handle;
-            
-            bool b1 = RegisterHotKey(handle, VK_MEDIA_NEXT_TRACK, 0, VK_MEDIA_NEXT_TRACK);
-            bool b2 = RegisterHotKey(handle, VK_MEDIA_PREV_TRACK, 0, VK_MEDIA_PREV_TRACK);
-            bool b3 = RegisterHotKey(handle, VK_MEDIA_STOP, 0, VK_MEDIA_STOP);
-            bool b4 = RegisterHotKey(handle, VK_MEDIA_PLAY_PAUSE, 0, VK_MEDIA_PLAY_PAUSE);
-            bool b5 = RegisterHotKey(handle, (int)Key.MediaPlayPause, 0, (int)Key.MediaPlayPause);
+
+            RegisterHotKey(handle, VK_VOLUME_DOWN, 0, MediaKeyVolumeDown);
+            RegisterHotKey(handle, VK_VOLUME_UP, 0, MediaKeyVolumeUp);
+            RegisterHotKey(handle, VK_MEDIA_NEXT_TRACK, 0, MediaKeyNext);
+            RegisterHotKey(handle, VK_MEDIA_PREV_TRACK, 0, MediaKeyPrevious);
+            RegisterHotKey(handle, VK_MEDIA_STOP, 0, MediaKeyStop);
+            RegisterHotKey(handle, VK_MEDIA_PLAY_PAUSE, 0, MediaKeyPlayPause);
 
             ComponentDispatcher.ThreadPreprocessMessage += ProcessKeyPress;
-            */
         }
 
         private void UnregisterGlobalMediaKeys()
         {
-            /*
             ComponentDispatcher.ThreadPreprocessMessage -= ProcessKeyPress;
             IntPtr handle = new WindowInteropHelper(this).Handle;
 
+            UnregisterHotKey(handle, VK_VOLUME_DOWN);
+            UnregisterHotKey(handle, VK_VOLUME_UP);
             UnregisterHotKey(handle, VK_MEDIA_NEXT_TRACK);
             UnregisterHotKey(handle, VK_MEDIA_PREV_TRACK);
             UnregisterHotKey(handle, VK_MEDIA_STOP);
             UnregisterHotKey(handle, VK_MEDIA_PLAY_PAUSE);
-            */
         }
 
         void ProcessKeyPress(ref MSG msg, ref bool handled)
         {
-            /*
-            //if (msg.message == WM_HOTKEY)
-            if (msg.message == WM_KEYDOWN)
+            if (msg.message == WM_HOTKEY)
             {
-                int i = 0;
+                if ((int)msg.wParam == MediaKeyPlayPause)
+                {
+                    // ...
+                }
             }
-            */
         }
     }
 }

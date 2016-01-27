603c6789b9df2b65a6e155d524d571d0104a4d4f
auremompd <auremompd@gmail.com>
2016-01-27 23:12:57 +0200

    Bump version to 0.8.0, update assembly copyright notice.

--- Auremo/Auremo/Properties/AssemblyInfo.cs
+++ Auremo/Auremo/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Auremo")]
-[assembly: AssemblyCopyright("Copyright © Mikko Teräs and Niilo Säämänen 2015")]
+[assembly: AssemblyCopyright("Copyright © Mikko Teräs and Niilo Säämänen 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
